330ae545136c4e3688f2d84ff9292f8db1704ffa
Rodrigo Rojas <rojaz.soliz@gmail.com>
2026-04-05 20:24:27 -0400

    tarea 1: Listas y array

---
+++ lista.py
@@ -0,0 +1,21 @@
+# Lista de ventas de equipos de computación
+
+ventas_lista = [
+    ["Laptop", 5, 800],      # producto, cantidad, precio
+    ["Mouse", 20, 15],
+    ["Teclado", 10, 25],
+    ["Monitor", 7, 200]
+]
+
+# Mostrar datos
+for venta in ventas_lista:
+    producto = venta[0]
+    cantidad = venta[1]
+    precio = venta[2]
+    total = cantidad * precio
+
+    print("Producto:", producto)
+    print("Cantidad:", cantidad)
+    print("Precio:", precio)
+    print("Total:", total)
+    print("-------------------")
